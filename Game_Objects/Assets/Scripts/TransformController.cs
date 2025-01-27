ef49f6ccfb6877b7f8ef0abb8c6d91f1e313e4e4
macey-s <11033951@uvu.edu>
2025-01-27 01:07:47 -0700

    lets go

--- Game_Objects/Assets/Scripts/TransformController.cs
+++ Game_Objects/Assets/Scripts/TransformController.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TransformController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
+        //Move the target GameObject
+        var x = Mathf.PingPong(Time.time, 3);
+        var p = new Vector3(0, x, 0);
+        transform.position = p;
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //Rotate the GameObject
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
     }
 }
