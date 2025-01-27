52aa8547c8724b5355edbf6b7a55584404d2cce7
macey-s <11033951@uvu.edu>
2025-01-26 23:55:02 -0700

    ignore this

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -8,9 +8,8 @@ public class TransformController : MonoBehaviour
         var x = Mathf.PingPong(Time.time, 3);
         var p = new Vector3(0, x, 0);
         transform.position = p;
-
-        //Rotate the target GameObject
-        transform.Rotate(new Vector3(0, 30, 0)) * Time.deltaTime);
+        
+        //Rotate the GameObject
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
     }
 }
-
