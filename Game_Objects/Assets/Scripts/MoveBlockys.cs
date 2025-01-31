ec638066dc894f43e38847356a5a844d767ace11
macey-s <11033951@uvu.edu>
2025-01-31 01:06:36 -0700

    Ground collision added

--- Game_Objects/Assets/Scripts/MoveBlockys.cs
+++ Game_Objects/Assets/Scripts/MoveBlockys.cs
@@ -6,7 +6,15 @@ public class ApplyForce : MonoBehaviour
 
     private void Start()
     {
+        // Applies force to object//
         rb = GetComponent<Rigidbody>();
         rb.AddForce(Vector3.right * 500);
+        
+        // Ground applies force to object//
+        void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log("Collision detected with " + collision.gameObject.name);
+        }
     }
 }
+
