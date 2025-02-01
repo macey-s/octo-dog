34d49be08ff7daf68189eff20f03b9415a1f4f18
macey-s <11033951@uvu.edu>
2025-02-01 15:03:47 -0700

    created bouncy ball script

---
+++ Game_Objects/Assets/Scripts/BouncyBall.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class BouncyBall : MonoBehaviour
+{
+    public float bounceForce = 3f;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
+    }
+}
