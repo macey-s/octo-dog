d9d9a789126c592f529a6e534dd513befd7006de
macey-s <11033951@uvu.edu>
2025-02-08 23:49:13 -0700

    made new animated scrip

---
+++ Game_Objects/Assets/Scripts/FlipTransform.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+
+public class FlipTransformBehavior : MonoBehaviour
+{
+    public KeyCode key1 = KeyCode.RightArrow, key2 = KeyCode.LeftArrow;
+    public float direction1 = 0, direction2 = 180;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(key1))
+        {
+            transform.rotation = Quaternion.Euler(0, direction1,0);
+        }
+
+        if (!Input.GetKeyDown(key2)) return;
+        transform.rotation = Quaternion.Euler(0, direction2, 0);
+    }
+}
+
