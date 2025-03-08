611947b40b84054f353f21ea7088dca0f5c1e9b2
macey-s <11033951@uvu.edu>
2025-03-08 15:54:53 -0700

    idk wtf im doing

---
+++ Game_Objects/Assets/Scripts/SimpleFloatData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Single Variables/SimpleFloatData")]
+public class SimpleFloatData : ScriptableObject
+{
+    public float value;
+    
+    public void UpdateValue(float amount)
+    {
+        value += amount;
+    }
+
+    public void SetValue(float amount)
+    {
+        value = amount;
+    }
+}
